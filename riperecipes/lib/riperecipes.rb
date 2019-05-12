2fb8125118165887ba1c0694b6a45ecc94280cf7
arpethel <arpethel@gmail.com>
2019-05-12 06:53:47 -0400

    preparing documents to build

--- riperecipes/lib/riperecipes.rb
+++ riperecipes/lib/riperecipes.rb
@@ -1,6 +1,24 @@
-require "riperecipes/version"
+# require "riperecipes/version"
+require 'pry'
+require 'open-uri'
 
 module Riperecipes
   class Error < StandardError; end
-  
+
+  class Scraper
+    # binding.pry
+    def something
+      puts 'hello'
+    end
+    
+    # def self.scrape_index_page
+    #   binding.pry
+    #   doc = Nokogiri::HTML(open("https://www.allrecipes.com/"))
+    #   puts 'hello'
+    #   binding.pry
+    #   # doc = Nokogiri::HTML(open(index_url))
+    # end
+
+  end
 end
+
--- riperecipes/spec/riperecipes_spec.rb
+++ riperecipes/spec/riperecipes_spec.rb
@@ -1,9 +1,18 @@
+require "spec_helper"
+
 RSpec.describe Riperecipes do
   it "has a version number" do
     expect(Riperecipes::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
+
+# RSpec.describe "Scraper" do
+#   describe "scrape_index_page" do
+#     it "is a class method that scrapes the recipes and returns and array of hashes..."
+#     index_url = "https://www.allrecipes.com/"
+#   end
+# end
