31c135079e9ef666aae36d272c5f793023b8f96d
arpethel <arpethel@gmail.com>
2019-05-12 21:16:53 -0400

    Deleted require 'pry'

--- riperecipes/lib/riperecipes.rb
+++ riperecipes/lib/riperecipes.rb
@@ -5,10 +5,6 @@ require_relative './riperecipes/cli'
 require 'nokogiri'
 require 'open-uri'
 
-# take this out before you publish!
-require 'pry'
-# require 'open-uri'
-
 # module Riperecipes
 #   class Error < StandardError; end
 # end
